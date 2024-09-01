f330fc0f772a36feba890699cd4e6f7c93cf70b4
Chris Wiegman <chris@chriswiegman.com>
2024-08-31 19:56:07 -0500

    Cleanup

--- shared/scripts/homebrew.sh
+++ shared/scripts/homebrew.sh
@@ -9,11 +9,8 @@ if test ! "$(command -v brew)"; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-if [ "$(uname)" = "Linux" ]; then
-    test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-    test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-fi
+# Launch Homebrew
+[ -s "/opt/homebrew/bin/brew" ] && eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew analytics off
 
--- shared/scripts/oh-my-z.sh
+++ shared/scripts/oh-my-z.sh
@@ -2,7 +2,10 @@
 
 echo "Setting up Oh My Zsh"
 
-# Setup Node on LTS
+# Setup oh-my-zsh
 if [ ! -d $HOME/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
+
+# Load oh-my-zsh
+source $ZSH/oh-my-zsh.sh
