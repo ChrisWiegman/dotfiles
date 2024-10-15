194f343c73c80f8dc84b4e96a8bee09156d9715a
Chris Wiegman <cwiegman@superfile.com>
2024-10-15 13:46:30 -0500

    Fix setup description.

--- setup.sh
+++ setup.sh
@@ -9,8 +9,8 @@ die () {
     exit 1
 }
 
-[ "$#" -eq 1 ] || die "Please invoke script with either "mac," "work" or "joy" as argument"
-echo $1 | grep -E -q '^(mac|work|joy)$' || die "Please invoke script with either "mac," "linux," "work" or "joy" as argument"
+[ "$#" -eq 1 ] || die "Please invoke script with either "mac," "work" or "joy" as the single argument"
+echo $1 | grep -E -q '^(mac|work|joy)$' || die "Please invoke script with either "mac," "work" or "joy" as the single argument"
 
 zsh $SHAREDPATH/setup.sh $SHAREDPATH $MACHINEPATH
 
