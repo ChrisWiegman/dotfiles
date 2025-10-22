e82962bd29f069d1de64091d95849b06c7b06c56
Chris Wiegman <chris@chriswiegman.com>
2025-10-22 16:37:43 -0500

    Update setup.sh

--- shared/setup.sh
+++ shared/setup.sh
@@ -1,22 +1,22 @@
-#!/bin/zsh
+#!/bin/sh
 
 SHAREDPATH=$1
 MACHINEPATH=$2
 
 if [[ $OSTYPE == darwin* ]]; then
-    zsh $SHAREDPATH/scripts/mac.sh
+    sh $SHAREDPATH/scripts/mac.sh
 fi
 
 if [[ $OSTYPE == linux* ]]; then
-    zsh $SHAREDPATH/scripts/linux.sh $SHAREDPATH
+    sh $SHAREDPATH/scripts/linux.sh $SHAREDPATH
 fi
 
-zsh $SHAREDPATH/scripts/homebrew.sh $MACHINEPATH
-zsh $SHAREDPATH/scripts/oh-my-z.sh
-zsh $SHAREDPATH/scripts/configs.sh $SHAREDPATH $MACHINEPATH
+sh $SHAREDPATH/scripts/homebrew.sh $MACHINEPATH
+sh $SHAREDPATH/scripts/oh-my-z.sh
+sh $SHAREDPATH/scripts/configs.sh $SHAREDPATH $MACHINEPATH
 
 # Run the local config if its available
-[ -s "$MACHINEPATH/setup.sh" ] && zsh $MACHINEPATH/setup.sh $MACHINEPATH
+[ -s "$MACHINEPATH/setup.sh" ] && sh $MACHINEPATH/setup.sh $MACHINEPATH
 
 # Create the code folder if we don't have it
 if [ ! -d $HOME/Code ]; then
