c6077b789b5f5690a3c8a87bd0021f8436010b1c
Chris Wiegman <chris@chriswiegman.com>
2025-10-22 16:40:55 -0500

    Update setup.sh

--- shared/setup.sh
+++ shared/setup.sh
@@ -11,14 +11,14 @@ if [[ $OSTYPE == linux* ]]; then
     sh $SHAREDPATH/scripts/linux.sh $SHAREDPATH
 fi
 
+# Create the code folder if we don't have it
+if [ ! -d $HOME/Code ]; then
+    mkdir -p $HOME/Code;
+fi
+
 sh $SHAREDPATH/scripts/homebrew.sh $MACHINEPATH
 sh $SHAREDPATH/scripts/oh-my-z.sh
 sh $SHAREDPATH/scripts/configs.sh $SHAREDPATH $MACHINEPATH
 
 # Run the local config if its available
 [ -s "$MACHINEPATH/setup.sh" ] && sh $MACHINEPATH/setup.sh $MACHINEPATH
-
-# Create the code folder if we don't have it
-if [ ! -d $HOME/Code ]; then
-    mkdir -p $HOME/Code;
-fi
