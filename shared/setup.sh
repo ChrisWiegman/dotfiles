57419ca4345b8bae4392f60b4c864b7becfeca1f
Chris Wiegman <chris@chriswiegman.com>
2025-10-22 16:49:21 -0500

    Update setup.sh

--- shared/setup.sh
+++ shared/setup.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 
-SHAREDPATH=$1
-MACHINEPATH=$2
+SHAREDPATH="$1"
+MACHINEPATH="$2"
 
-if [[ $OSTYPE == darwin* ]]; then
-    sh $SHAREDPATH/scripts/mac.sh
+OS="$(uname)"
+
+if [ "$OS" = "Darwin" ]; then
+    sh "$SHAREDPATH/scripts/mac.sh"
 fi
 
-if [[ $OSTYPE == linux* ]]; then
-    sh $SHAREDPATH/scripts/linux.sh $SHAREDPATH
+if [ "$OS" = "Linux" ]; then
+    sh "$SHAREDPATH/scripts/linux.sh" "$SHAREDPATH"
 fi
 
 # Create the code folder if we don't have it
-if [ ! -d $HOME/Code ]; then
-    mkdir -p $HOME/Code;
+if [ ! -d "$HOME/Code" ]; then
+    mkdir -p "$HOME/Code"
 fi
 
-sh $SHAREDPATH/scripts/homebrew.sh $MACHINEPATH
-sh $SHAREDPATH/scripts/oh-my-z.sh
-sh $SHAREDPATH/scripts/configs.sh $SHAREDPATH $MACHINEPATH
+sh "$SHAREDPATH/scripts/homebrew.sh" "$MACHINEPATH"
+sh "$SHAREDPATH/scripts/oh-my-z.sh"
+sh "$SHAREDPATH/scripts/configs.sh" "$SHAREDPATH" "$MACHINEPATH"
 
 # Run the local config if its available
-[ -s "$MACHINEPATH/setup.sh" ] && sh $MACHINEPATH/setup.sh $MACHINEPATH
+if [ -s "$MACHINEPATH/setup.sh" ]; then
+    sh "$MACHINEPATH/setup.sh" "$MACHINEPATH"
+fi
