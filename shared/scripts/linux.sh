62d1c9dc826079f457559f35081b6300e2bb0fb0
Chris Wiegman <chris@chriswiegman.com>
2025-10-27 15:59:00 -0500

    Update linux.sh

--- shared/scripts/linux.sh
+++ shared/scripts/linux.sh
@@ -19,16 +19,13 @@ sudo apt-get install -y \
     build-essential
 sudo apt-get clean
 
-# Install VSCode
+# Setup VSCode Repo
 echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg
 rm -f microsoft.gpg
 
 sudo cp $CONFIG_DIR/vscode.sources /etc/apt/sources.list.d
-
-sudo apt-get update
-sudo apt-get install -y code
 
 # Setup Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -44,7 +41,29 @@ sudo snap remove --purge firefox
 
 set -eu
 
-LANG=C snap list --all | awk '/disabled/{print $1, $3}' |
+LANG=C sudo snap list --all | awk '/disabled/{print $1, $3}' |
     while read snapname revision; do
-        snap remove "$snapname" --revision="$revision"
+        sudo snap remove "$snapname" --revision="$revision"
     done
+
+# Setup 1Password repo
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+
+# Setup Signal repo
+wget -qO- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg;
+cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+wget -qO- signal-desktop.sources https://updates.signal.org/static/desktop/apt/signal-desktop.sources;
+cat signal-desktop.sources | sudo tee /etc/apt/sources.list.d/signal-desktop.sources > /dev/null
+rm signal-desktop-keyring.gpg
+
+# Install apps
+sudo apt-get update
+sudo apt-get install -y \
+    code \
+    1password \
+    signal-desktop
