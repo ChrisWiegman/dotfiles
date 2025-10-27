70c9332120ebe314d89c59e5541e6ddf06d77aec
Chris Wiegman <chris@chriswiegman.com>
2025-10-27 15:39:19 -0500

    Update linux.sh

--- shared/scripts/linux.sh
+++ shared/scripts/linux.sh
@@ -30,6 +30,21 @@ sudo cp $CONFIG_DIR/vscode.sources /etc/apt/sources.list.d
 sudo apt-get update
 sudo apt-get install -y code
 
+# Setup Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt install -f -y
+rm google-chrome-stable_current_amd64.deb
+
 # Setup Flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
+# Remove Firefox
+sudo snap remove --purge firefox
+
+set -eu
+
+LANG=C snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
