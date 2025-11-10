074ccedbaa28181012a27058b4703857baa5fadd
Chris Wiegman <chris.wiegman@fingerprint.com>
2025-11-10 14:52:14 -0600

    More setup

--- machines/work/setup.sh
+++ machines/work/setup.sh
@@ -3,13 +3,20 @@
 SHAREDPATH=$1
 MACHINEPATH=$2
 
-CONFIG_DIR=$SHAREDPATH/configs
+M_CONFIG_DIR=$MACHINEPATH/configs
 
-echo "Setting up a local SSH config file."
+# Setup SSH configs
 rm -f $HOME/.ssh/config
-mv $CONFIG_DIR/.ssh/config $HOME/.ssh/config
+ln -s $M_CONFIG_DIR/.ssh/config $HOME/.ssh/config
 chmod 0700 $HOME/.ssh
 chmod 0600 $HOME/.ssh/config
+
+# Setup Git configs
+echo "Setting up a local GIT config file."
+if [ ! -f $HOME/.gitconfig ] || [ ! $(grep -q "signingkey" "$HOME/.gitconfig") ]; then
+    rm -f $HOME/.gitconfig
+    cp $M_CONFIG_DIR/.gitconfig $HOME/.gitconfig
+fi
 
 # Load ZSH and its config
 zsh
